dac7f3ded48ad22752f53686800c36608ff656c3
LOLOaHY <ejekdjdjed@gmail.com>
2024-09-13 21:39:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import ffmpeg
 
 app = Flask(__name__)
 
+cookie_file_path = 'cookie_file.json'
 # المسار الذي سيتم حفظ الفيديوهات فيه
 DOWNLOAD_PATH = os.path.join(os.path.dirname(__file__), 'uploads')
 if not os.path.exists(DOWNLOAD_PATH):
@@ -21,7 +22,9 @@ def get_formats():
         return jsonify({'error': 'رابط الفيديو مفقود'}), 400
 
     try:
-        ydl_opts = {}
+        ydl_opts = {
+            'cookiefile': cookie_file_path,
+        }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=False)
             formats = info_dict.get('formats', [])
@@ -46,6 +49,7 @@ def get_formats():
     except Exception as e:
         return jsonify({'error': f"حدث خطأ أثناء جلب الجودات: {str(e)}"}), 500
 
+
 @app.route('/download', methods=['POST'])
 def download_video():
     data = request.form
@@ -57,6 +61,7 @@ def download_video():
     try:
         # إعدادات yt-dlp لتنزيل الفيديو والصوت
         ydl_opts = {
+            'cookiefile': cookie_file_path,
             'outtmpl': os.path.join(DOWNLOAD_PATH, '%(title)s.%(ext)s'),
             'format': f'{format_id}+bestaudio/best',
             'merge_output_format': 'mp4',
