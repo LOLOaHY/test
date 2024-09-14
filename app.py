af0078380ad257df0f2d81485fa24e5d934b01ac
LOLOaHY <ejekdjdjed@gmail.com>
2024-09-14 11:47:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def get_formats():
 
     try:
 
-        ydl_opts = {'chrome:windows-10',}
+        ydl_opts = {}
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=False)
             formats = info_dict.get('formats', [])
@@ -59,7 +59,6 @@ def download_video():
         # إعدادات yt-dlp لتنزيل الفيديو والصوت
         
         ydl_opts = {
-            'impersonate': 'chrome:windows-10',
             'outtmpl': os.path.join(DOWNLOAD_PATH, '%(title)s.%(ext)s'),
             'format': f'{format_id}+bestaudio/best',
             'merge_output_format': 'mp4',
