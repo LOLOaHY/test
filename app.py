2ca66d150c3f01aa2bd23029b2b58650690da881
LOLOaHY <ejekdjdjed@gmail.com>
2024-09-16 11:21:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def get_formats():
         return jsonify({'error': 'رابط الفيديو مفقود'}), 400
 
     try:
-        ydl_opts = {'cookiefile':'/workspace/cookies.txt','ffmpeg_location':'/workspace/ffmpeg-git-20240629-amd64-static/ffmpeg'}
+        ydl_opts = {'cookiefile':'/workspace/cookies.txt','ffmpeg_location':'/workspace/ffm/ffmpeg-git-20240629-amd64-static/ffmpeg'}
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=False)
             formats = info_dict.get('formats', [])
@@ -58,7 +58,7 @@ def download_video():
         # إعدادات yt-dlp لتنزيل الفيديو والصوت
         ydl_opts = {
             'cookiefile':'/workspace/cookies.txt',
-            'ffmpeg_location':'/workspace/ffmpeg-git-20240629-amd64-static/ffmpeg',
+            'ffmpeg_location':'/workspace/ffm/ffmpeg-git-20240629-amd64-static/ffmpeg',
             'outtmpl': os.path.join(DOWNLOAD_PATH, '%(title)s.%(ext)s'),
             'format': f'{format_id}+bestaudio/best',
             'merge_output_format': 'mp4',
